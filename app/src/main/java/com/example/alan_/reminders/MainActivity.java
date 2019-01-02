bdef8221e67546edf228f529c9a01e3457d36db8
alan_ <alan_hernandezgarcia@hotmail.com>
2019-01-01 19:15:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/alan_/reminders/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.alan_.reminders;
+
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
+    }
+
+}
